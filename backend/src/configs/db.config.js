763f4420efad93d193199f866e7695663ca14f90
zildanpandarusm <zildanmarginata@gmail.com>
2024-03-07 21:31:09 +0700

    add produk

--- backend/src/configs/db.config.js
+++ backend/src/configs/db.config.js
@@ -1,4 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-  "mongodb+srv://zamzam:yayang99@cluster0.kq6zdgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const mongoDBURL = 'mongodb+srv://zamzam:yayang99@cluster0.kq6zdgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,17 @@
-import express from "express";
-import mongoose from "mongoose";
-import { PORT, mongoDBURL } from "./configs/db.config.js";
-import toko_routes from "./routes/toko.routes.js";
-import { notFound, errorHandler } from "./middleware/error.middleware.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import { PORT, mongoDBURL } from './configs/db.config.js';
+import toko_routes from './routes/toko.routes.js';
+import produk_routes from './routes/produk.routes.js';
+import { notFound, errorHandler } from './middleware/error.middleware.js';
 
-import Cors from "cors";
+import Cors from 'cors';
 
 export const app = express();
 
 const corsOptions = {
-  origin: "*", // Alamat domain React
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: '*', // Alamat domain React
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Mengizinkan pengiriman cookies
   optionsSuccessStatus: 204,
 };
@@ -18,19 +19,20 @@ const corsOptions = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   console.log(req);
-  return res.status(234).send("hello");
+  return res.status(234).send('hello');
 });
 
 // API Toko
-app.use("/api/toko", toko_routes);
+app.use('/api/toko', toko_routes);
+app.use('/api/produk', produk_routes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 mongoose.connect(mongoDBURL).then(() => {
-  console.log("App connected to database");
+  console.log('App connected to database');
   app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
   });
