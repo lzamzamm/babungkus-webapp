9ef9349d43d294a48043ec3a0e24df7e7eaef218
Muhammad <muhammadahjahh@gmail.com>
2024-03-09 18:43:22 +0700

    devs branch

--- front-end/src/routes/App.jsx
+++ front-end/src/routes/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import BerandaPage from '../pages/BerandaPage'
+import ContactPage from '../pages/ContactPage'
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' Component={BerandaPage} />
+        <Route path='/contact' Component={ContactPage} />
       </Routes>
     </div>
   )
