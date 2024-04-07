a72c81523013369fe20465fc7a93afe7cdddd631
chalisun <chalisun981@gmail.com>
2024-04-08 00:00:08 +0700

    dashboarpage

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,62 +1,67 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
 import LogoBabungkus from '/assets/images/logo.png';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   
   const navItems = [
-    { label: 'Produk', href: '/', subItems: [{ label: 'Makanan', href: '/' }, { label: 'Minuman', href: '/' }] },
-    { label: 'Outlet', href: '/' },
-    { label: 'Kontak', href: '/contact' }
+    { label: 'Produk', href: '/produk', subItems: [{ label: 'Makanan', href: '/produk/makanan' }, { label: 'Minuman', href: '/produk/minuman' }, { label: 'Pakan', href: '/produk/pakan' }] },
+    { label: 'Toko', href: '/toko' },
+    { label: 'Tentang Kami', href: '/tentangkami' }
   ];
 
   const buttonItems = [
-    { label: 'Masuk', href: '/login' },
-    { label: 'Daftar', href: '/register' }
+    { label: 'Masuk', href: '/masuk' },
+    { label: 'Daftar', href: '/daftar' }
   ];
 
   return (
-    <div className='w-full font-poppins md:text-lg flex justify-center items-center py-4 px-[5%] fixed shadow-mini-xlx sm:shadow-xlx z-50 bg-white'>
-      <div className='flex justify-between w-full'>
-        <div className='flex items-center gap-[10%] w-[60%]'>
+    <div className="w-full fixed shadow-mini-xlx sm:shadow-xlx z-50 md:text-lg font-poppins bg-white">
+      <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:flex-row md:items-center md:justify-between md:px-6 lg:px-8">
+        <div className="p-4 flex flex-row items-center justify-between">
           <div className='flex justify-center items-center w-[50px]'>
             <img src={LogoBabungkus} className='w-full h-full' alt="Logo Babungkus" />
-          </div>
-          <div className='flex items-center justify-center hover:text-primary'>
-            <button
-              onClick={() => setIsOpen((prev) => !prev)}
-              className='flex items-center justify-center'>
-              Produk
-              {!isOpen ? (
-                <IoIosArrowDown className='pl-[10%]' />
-              ) : (
-                <IoIosArrowUp className='pl-[10%]' />
-              )}
-            </button>
-            {isOpen && (
-              <div className='absolute p-[10%] bg-slate-400'>
-                {navItems[0].subItems.map((item, index) => (
-                  <Link key={index} to={item.href}>{item.label}</Link>
-                ))}
-              </div>
-            )}
-          </div>
-          {navItems.slice(1).map((item, index) => (
-            <Link key={index} to={item.href} className='hover:text-primary'>{item.label}</Link>
-          ))}
+          </div>          
+          <button onClick={() => setIsOpen(!isOpen)} className="md:hidden rounded-lg focus:outline-none focus:shadow-outline">
+            {isOpen ? <HiOutlineX className="w-6 h-6" /> : <HiOutlineMenuAlt3 className="w-6 h-6" />}
+          </button>
         </div>
-        <div className='flex items-center justify-between text-white gap-5'>
-          {buttonItems.map((item, index) => (
-            <Link key={index} to={item.href} className='hover:bg-primary-dark px-5 py-2 text-center bg-primary rounded-5 transition duration-300 ease-in-out'>
-              {item.label}
-            </Link>
-          ))}
-        </div>
-        <Link to='/detail-product'>det pro</Link>
-        <Link to='/riwayat'>det riw</Link>
-        <Link to='/akun'>det ak</Link>
+        <nav className={`flex-col h-screen md:h-auto flex-grow pb-4 md:pb-0 md:flex justify-between md:justify-end md:flex-row transition duration-300 ease-in-out ${isOpen ? 'flex' : 'hidden'}`}>
+          <div className="md:flex md:items-center md:justify-between w-full">
+            <div className="md:flex md:items-center gap-2">
+              {navItems.map((item, index) => (
+                <div key={index} className="relative">
+                {item.label === 'Produk' ? (
+                  <button onClick={() => setDropdownOpen((prev) => !prev)} className="flex w-full items-center px-4 py-2 mt-2 rounded-lg md:mt-0 hover:text-primary focus:text-primary focus:outline-none focus:shadow-outline">
+                  {item.label}
+                  <IoIosArrowDown className={`inline w-4 h-4 ml-4 transition-transform duration-200 transform ${dropdownOpen ? 'rotate-180' : 'rotate-0'}`} />
+                </button>
+                  ) : (
+                    <Link to={item.href} className="flex px-4 py-2 mt-2 rounded-lg md:mt-0 hover:text-primary focus:text-primary">
+                      {item.label}
+                    </Link>
+                  )}
+                  {dropdownOpen && item.subItems && item.subItems.length > 0 && (
+                    <div className="dropdown-menu text-base px-2 py-2 md:absolute right-0 w-full mt-0 md:mt-2 rounded-lg z-10 md:border bg-white">
+                      {item.subItems.map((subItem, subIndex) => (
+                        <a key={subIndex} className="block px-4 py-2 rounded-lg hover:text-primary focus:text-primary focus:outline-none focus:shadow-outline" href={subItem.href}>{subItem.label}</a>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="md:flex md:items-center gap-2 mt-4 md:mt-0">
+              {buttonItems.map((item, index) => (
+                <Link key={index} to={item.href} className="flex  justify-center text-white px-4 py-2 mt-2 rounded-lg md:mt-0 hover:bg-primary-dark bg-primary transition duration-300 ease-in-out">{item.label}</Link>
+              ))}
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
   );
--- front-end/src/components/PersonalForm.jsx
+++ front-end/src/components/PersonalForm.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PersonalForm = () => {
   return (
-    <div className="p-5 w-full text-gray-800" style={{ fontFamily: 'Poppins, sans-serif' }}>
-      <h2 className="text-2xl mb-2">Data Diri</h2>
-      <hr className="mb-10" style={{ height: '2px', backgroundColor: '#000', border: 'none' }} />
-      <form className="max-w-2xl">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-l  mb-2" htmlFor="NamaLengkap">
+    <div className="p-3 w-full text-gray-800 mt-10 md:mt-2 lg:mt-1" style={{ fontFamily: 'Poppins, sans-serif' }}>
+      <h2 className="lg:text-2xl md:text-xl sm:text-base mb-2">Data Diri</h2>
+      <hr className="mb-5 sm:mb-10" style={{ height: '2px', backgroundColor: '#000', border: 'none' }} />
+      <form className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl">
+        <div className="mb-2 sm:mb-3 text-sm sm:text-lg">
+          <label className="block text-gray-700 mb-2" htmlFor="NamaLengkap">
             Nama Lengkap
           </label>
           <input
@@ -17,7 +17,7 @@ const PersonalForm = () => {
             type="text"
           />
         </div>
-        <div className="mb-2">
+        <div className="mb-2 sm:mb-3 text-sm sm:text-lg">
           <label className="block text-gray-700 text-l mb-2" htmlFor="NamaPengguna">
             Nama Pengguna
           </label>
@@ -28,7 +28,7 @@ const PersonalForm = () => {
             type="text"
           />
         </div>
-        <div className="mb-2">
+        <div className="mb-2 sm:mb-3 text-sm sm:text-lg">
           <label className="block text-gray-700 text-l mb-2 mr-2" htmlFor="Email">
             Email
           </label>
@@ -39,7 +39,7 @@ const PersonalForm = () => {
             type="email"
           />
         </div>
-        <div className="mb-2">
+        <div className="mb-2 sm:mb-3 text-sm sm:text-lg">
           <label className="block text-gray-700 text-l mb-2 mr-2" htmlFor="Telpon">
             Telpon
           </label>
@@ -51,7 +51,7 @@ const PersonalForm = () => {
           />
           <p className="text-gray-600 text-xs italic">Masukkan nomor telpon yang juga merupakan nomor WhatsApp</p>
         </div>
-        <button className="bg-primary hover:bg-amber-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        <button className="bg-primary hover:bg-amber-700 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
           Update
         </button>
       </form>
