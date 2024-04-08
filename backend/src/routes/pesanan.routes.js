9bac548831d6bde616311a603e71cdb0c0f78011
zildanpandarusm <zildanmarginata@gmail.com>
2024-04-08 12:53:58 +0700

    Fix route pesanan

--- backend/src/routes/pesanan.routes.js
+++ backend/src/routes/pesanan.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createPesanan, getPesananAll, getPesananById, updatePesanan, deletePesanan, getPesananAllCurrentUser } from '../controllers/pesanan.controller.js';
+import { protect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
@@ -7,7 +8,7 @@ router.post('/', createPesanan);
 
 router.get('/', getPesananAll);
 
-router.get('/current/user', getPesananAllCurrentUser);
+router.get('/current/user', protect, getPesananAllCurrentUser);
 
 router.get('/:id', getPesananById);
 
