dc98c6c8342e87ce8ed1bdcd3c99f6cbb958dcd1
lzamzamm <notlucy78@gmail.com>
2024-04-09 15:07:39 +0700

    update cookie 2

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,16 +5,17 @@ import asyncHandler from "express-async-handler";
 import User from "../models/user.model.js";
 
 import "dotenv/config";
+import Cookies from "js-cookie";
 
 const protect = asyncHandler(async (req, res, next) => {
-
-  const token = req.cookies.jwt;
+  var token = req.cookies.jwt;
   console.log(token);
-  
-  if (!token){
+
+  if (!token) {
     res.status(401);
     throw new Error("Unauthorized");
   }
+  // var token = cookie.split("=")[1];
 
   //console.log(token);
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
