bcfcd1b6a518b92cdd9cef6450935fbb7acc77f9
zildanpandarusm <zildanmarginata@gmail.com>
2024-04-09 11:00:16 +0700

    fix produk

--- backend/src/routes/laporan.routes.js
+++ backend/src/routes/laporan.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createLaporan, getLaporanAll, getLaporanById, deleteLaporan } from '../controllers/laporan.controller.js';
+import { upload } from '../utils/upload/laporan.img.js';
 
 const router = express.Router();
 
-router.post('/', createLaporan);
+router.post('/', upload.fields([{ name: 'file', maxCount: 1 }, { name: 'data' }]), createLaporan);
 
 router.get('/', getLaporanAll);
 
--- backend/src/routes/produk.routes.js
+++ backend/src/routes/produk.routes.js
@@ -1,25 +1,19 @@
-import express from "express";
-import {
-  createProduk,
-  deleteProduk,
-  getProdukAll,
-  getProdukById,
-  getProdukByKategori,
-  UpdateProduk,
-} from "../controllers/produk.controller.js";
+import express from 'express';
+import { createProduk, deleteProduk, getProdukAll, getProdukById, getProdukByKategori, UpdateProduk } from '../controllers/produk.controller.js';
+import { upload } from '../utils/upload/produk.img.js';
 
 const router = express.Router();
 
-router.post("/", createProduk);
+router.post('/', upload.fields([{ name: 'file', maxCount: 1 }, { name: 'data' }]), createProduk);
 
-router.get("/", getProdukAll);
+router.get('/', getProdukAll);
 
-router.get("/kategori", getProdukByKategori);
+router.get('/kategori', getProdukByKategori);
 
-router.get("/:id", getProdukById);
+router.get('/:id', getProdukById);
 
-router.patch("/:id", UpdateProduk);
+router.patch('/:id', upload.fields([{ name: 'file', maxCount: 1 }, { name: 'data' }]), UpdateProduk);
 
-router.delete("/:id", deleteProduk);
+router.delete('/:id', deleteProduk);
 
 export default router;
