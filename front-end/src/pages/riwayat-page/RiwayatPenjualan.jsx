77628c5d95820805a778ee52eb3c960b12ca8e18
Muhammad <muhammadahjahh@gmail.com>
2024-04-10 23:02:21 +0800

    .

--- front-end/src/pages/riwayat-page/RiwayatPenjualan.jsx
+++ front-end/src/pages/riwayat-page/RiwayatPenjualan.jsx
@@ -13,13 +13,11 @@ function RiwayatPenjualan() {
   const [toko, setToko] = useState({});
   const [produk, setProd] = useState({});
 
-    const getData = async () => {
-        const response = await axios.get('http://localhost:5555/api/pesanan/current/toko', {
-            withCredentials: true,
-        }); 
-        setData(response.data.data);
-        console.log(response.data.data)
-    };
+  const getData = async () => {
+    const response = await axios.get('http://localhost:5555/api/pesanan/current/user'); // ini BELOMMM
+    setData(response.data.data);
+    console.log(response.data.data)
+  };
 
   // const getData = async () => {
   //   const response = await axios.get('http://localhost:5555/api/pesanan/current/user');
@@ -27,13 +25,11 @@ function RiwayatPenjualan() {
   //   console.log(response.data.data)
   // };
 
-    const getToko = async () => {
-        const response = await axios.get('http://localhost:5555/api/toko', {
-            withCredentials: true,
-        });
-        setToko(response.data.data);
-        console.log(response.data.data)
-    }
+  const getToko = async () => {
+    const response = await axios.get('http://localhost:5555/api/toko');
+    setToko(response.data.data);
+    console.log(response.data.data)
+  }
 
   useEffect(() => {
     getData();
@@ -58,57 +54,39 @@ function RiwayatPenjualan() {
             </div>
           </div>
         </div>
-        <div className="hp:h-auto lg:w-[40%]">
-          <p className="h-[4%] text-[1.75vmax] font-semibold lg:text-center">
-            Penjualan
-          </p>
-          <ul className="flex h-auto flex-col gap-[2vh] rounded-[0.5vmax]">
+        <div className='hp:h-auto lg:w-[40%]'>
+          <p className='font-semibold lg:text-center h-[4%] text-[1.75vmax]'>Penjualan</p>
+          <ul className='h-auto rounded-[0.5vmax] flex flex-col gap-[2vh]'>
             {data.map((item) => (
-              <li className="h-[17vmax] rounded-[0.5vmax] bg-gray-100">
-                <div className="flex h-[20%] flex-row justify-between rounded-t-[0.5vmax] bg-slate-300 p-[1.5%]">
-                  <div className="flex w-[60%] flex-row items-center">
-                    <RiRecycleLine className="lg:text-[2.5vmax]" />
-                    <div className="pl-[5%] text-[1.5vmax]">
-                      {item.createdAt}
+              <li className='h-[17vmax] bg-gray-100 rounded-[0.5vmax]'>
+                <div className='flex flex-row justify-between h-[20%] bg-slate-300 p-[1.5%] rounded-t-[0.5vmax]'>
+                  <div className='flex flex-row items-center w-[60%]'>
+                    <RiRecycleLine className='lg:text-[2.5vmax]' />
+                    <div className='text-[1.5vmax] pl-[5%]'>{item.createdAt}</div>
+                  </div>
+                  <div className='text-[1.25vmax] pt-[0.5%] pb-[0.5%] pl-[1%] pr-[1%] bg-orange-300 rounded-[0.5vmax]'>{item.status_pembeli}</div>
+                </div>
+                <Link to='/'>
+                  <div className='h-[80%] p-[3%] hover:bg-slate-200'>
+                    <div className='flex flex-row h-[80%]'>
+                      <img className='h-[70%] w-[20%] rounded-[1vmax]' src={Food_1} />
+                      <div className='flex flex-col pl-[5%] w-[75%]'>
+                        <p className='font-light text-[1.75vmax]'>
+                          {item.info_produk.nama}
+                        </p>
+                        <p className='text-[#777777] text-[1.4vmax]'>Makanan</p>
+                      </div>
                     </div>
+                    <p className='h-[20%] text-[1.5vmax] text-end'><span className='text-[1.25vmax] pr-[2%]'>Total Pesanan </span> Rp {item.harga_total} </p>
                   </div>
-                  <div className="rounded-[0.5vmax] bg-orange-300 pb-[0.5%] pl-[1%] pr-[1%] pt-[0.5%] text-[1.25vmax]">
-                    {item.status_pembeli}
-                  </div>
-                </div>
-                <div className='hp:h-auto lg:w-[40%]'>
-                    <p className='font-semibold lg:text-center h-[4%] text-[1.75vmax]'>Penjualan</p>
-                    <ul className='h-auto rounded-[0.5vmax] flex flex-col gap-[2vh]'>
-                        {data.map((item) => (
-                            <li className='h-[17vmax] bg-gray-100 rounded-[0.5vmax]'>
-                                <div className='flex flex-row justify-between h-[20%] bg-slate-300 p-[1.5%] rounded-t-[0.5vmax]'>
-                                    <div className='flex flex-row items-center w-[60%]'>
-                                        <RiRecycleLine className='lg:text-[2.5vmax]' />
-                                        <div className='text-[1.5vmax] pl-[5%]'>{item.createdAt}</div>
-                                    </div>
-                                    <div className='text-[1.25vmax] pt-[0.5%] pb-[0.5%] pl-[1%] pr-[1%] bg-orange-300 rounded-[0.5vmax]'>{item.status_pembeli}</div>
-                                </div>
-                                <Link to='/'>
-                                    <div className='h-[80%] p-[3%] hover:bg-slate-200'>
-                                        <div className='flex flex-row h-[80%]'>
-                                            <img className='h-[70%] w-[20%] rounded-[1vmax]' src={Food_1} />
-                                            <div className='flex flex-col pl-[5%] w-[75%]'>
-                                                <p className='font-light text-[1.75vmax]'>
-                                                    {item.info_produk.nama}
-                                                </p>
-                                                <p className='text-[#777777] text-[1.4vmax]'>{item.info_produk.kategori}</p>
-                                            </div>
-                                        </div>
-                                        <p className='h-[20%] text-[1.5vmax] text-end'><span className='text-[1.25vmax] pr-[2%]'>Total Pesanan </span> Rp {item.harga_total} </p>
-                                    </div>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default RiwayatPenjualan;
