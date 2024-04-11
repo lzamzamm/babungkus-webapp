c839032c16e5a47c070f7754f1e6b5edc53ad8e6
eka <21106050023@student.uin-suka.ac.id>
2024-04-11 17:18:04 +0700

    feat: navbar

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { IoIosArrowDown } from "react-icons/io";
 import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
 import LogoBabungkus from "/assets/images/logo.png";
+import { FaEyeLowVision } from "react-icons/fa6";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,16 +23,40 @@ function Navbar() {
 
   const navItems = [
     {
+      label: "Dashboard",
+      href: "/admin/dashboard",
+      visibleForAdmin: true,
+    },
+    {
+      label: "Profil",
+      href: "/akun",
+      visibleForUser: true,
+    },
+    {
+      label: "Produk Saya",
+      href: "/akun/produk",
+      visibleForUser: true,
+    },
+    {
       label: "Produk",
       href: "/produk",
+      visibleForGuest: true,
       subItems: [
         { label: "Makanan", href: "/produk/makanan" },
         { label: "Minuman", href: "/produk/minuman" },
         { label: "Pakan", href: "/produk/pakan" },
       ],
     },
-    { label: "Toko", href: "/toko" },
-    { label: "Tentang Kami", href: "/tentangkami" },
+    {
+      label: "Toko",
+      href: "/toko",
+      visibleForGuest: true,
+    },
+    {
+      label: "Tentang Kami",
+      href: "/tentangkami",
+      visibleForGuest: true,
+    },
   ];
 
   const buttonItems = [
@@ -78,48 +103,58 @@ function Navbar() {
         >
           <div className="w-full md:flex md:items-center md:justify-between">
             <div className="gap-2 md:flex md:items-center">
-              {navItems.map((item, index) => (
-                <div key={index} className="relative">
-                  {item.label === "Produk" ? (
-                    <button
-                      onClick={() => setDropdownOpen((prev) => !prev)}
-                      className="focus:shadow-outline mt-2 flex w-full items-center rounded-lg px-4 py-2 hover:text-primary focus:text-primary focus:outline-none md:mt-0"
-                    >
-                      {item.label}
-                      <IoIosArrowDown
-                        className={`ml-4 inline h-4 w-4 transform transition-transform duration-200 ${
-                          dropdownOpen ? "rotate-180" : "rotate-0"
-                        }`}
-                      />
-                    </button>
-                  ) : (
-                    <Link
-                      to={item.href}
-                      className="mt-2 flex rounded-lg px-4 py-2 hover:text-primary focus:text-primary md:mt-0"
-                    >
-                      {item.label}
-                    </Link>
-                  )}
-                  {dropdownOpen &&
-                    item.subItems &&
-                    item.subItems.length > 0 && (
-                      <div className="dropdown-menu right-0 z-10 mt-0 w-full rounded-lg bg-white px-2 py-2 text-base md:absolute md:mt-2 md:border">
-                        {item.subItems.map((subItem, subIndex) => (
-                          <Link
-                            key={subIndex}
-                            className="focus:shadow-outline block rounded-lg px-4 py-2 hover:text-primary focus:text-primary focus:outline-none"
-                            to={subItem.href}
-                          >
-                            {subItem.label}
-                          </Link>
-                        ))}
-                      </div>
-                    )}
-                </div>
-              ))}
+              {navItems.map((item, index) => {
+                if (
+                  (item.visibleForAdmin && userRole === "Admin") ||
+                  (item.visibleForUser && userRole === "User") ||
+                  (item.visibleForGuest && !userRole)
+                ) {
+                  return (
+                    <div key={index} className="relative">
+                      {item.label === "Produk" ? (
+                        <button
+                          onClick={() => setDropdownOpen((prev) => !prev)}
+                          className="focus:shadow-outline mt-2 flex w-full items-center rounded-lg px-4 py-2 hover:text-primary focus:text-primary focus:outline-none md:mt-0"
+                        >
+                          {item.label}
+                          <IoIosArrowDown
+                            className={`ml-4 inline h-4 w-4 transform transition-transform duration-200 ${
+                              dropdownOpen ? "rotate-180" : "rotate-0"
+                            }`}
+                          />
+                        </button>
+                      ) : (
+                        <Link
+                          to={item.href}
+                          className="mt-2 flex rounded-lg px-4 py-2 hover:text-primary focus:text-primary md:mt-0"
+                        >
+                          {item.label}
+                        </Link>
+                      )}
+                      {dropdownOpen &&
+                        item.subItems &&
+                        item.subItems.length > 0 && (
+                          <div className="dropdown-menu right-0 z-10 mt-0 w-full rounded-lg bg-white px-2 py-2 text-base md:absolute md:mt-2 md:border">
+                            {item.subItems.map((subItem, subIndex) => (
+                              <Link
+                                key={subIndex}
+                                className="focus:shadow-outline block rounded-lg px-4 py-2 hover:text-primary focus:text-primary focus:outline-none"
+                                to={subItem.href}
+                              >
+                                {subItem.label}
+                              </Link>
+                            ))}
+                          </div>
+                        )}
+                    </div>
+                  );
+                } else {
+                  return null;
+                }
+              })}
             </div>
             <div className="mt-4 gap-2 md:mt-0 md:flex md:items-center">
-              {userRole === "User" && (
+              {userRole && (
                 <button
                   onClick={handleLogout}
                   className="mt-2 w-full flex justify-center rounded-lg bg-primary px-4 py-2 text-white transition duration-300 ease-in-out hover:bg-primary-dark md:mt-0"
