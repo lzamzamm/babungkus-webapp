2e9c80b3d591a36c34358bbff7f273e76b6dd2be
lzamzamm <notlucy78@gmail.com>
2024-04-11 21:44:03 +0700

    update confg

--- front-end/src/utils/config.jsx
+++ front-end/src/utils/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-    BASE_URL: "https://babungkus-api.vercel.app",
+    BASE_URL: "http://localhost:5555",
     IMAGE_BASE_URL: "https://babungkus-api.vercel.app/assets/images", // Ganti dengan URL base gambar kamu
 };
   
